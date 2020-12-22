801a387c592b70b376f3d72829a6c655afa92532
Mathias Gnadlinger <mathias.gnadlinger@htl-wels.at>
2020-12-22 20:31:29 +0100

    Button, TextField, TextArea einfügen

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,6 +1,41 @@
 package sample;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
 public class Controller
 {
+
+
+    @FXML private Button btn0;
+    @FXML private Button btn1;
+    @FXML private Button btn2;
+    @FXML private Button btn3;
+    @FXML private Button btn4;
+    @FXML private Button btn5;
+    @FXML private Button btn6;
+    @FXML private Button btn7;
+    @FXML private Button btn8;
+    @FXML private Button btn9;
+
+    @FXML private Button btnComma;
+    @FXML private Button btnPlus_Minus;
+    @FXML private Button btnEnter;
+    @FXML private Button btnClear;
+    @FXML private Button btnClearEntry;
+    @FXML private Button btnPlus;
+    @FXML private Button btnMinus;
+    @FXML private Button btnMultiply;
+    @FXML private Button btnDivide;
+    @FXML private Button btnReciprocal;
+    @FXML private Button btnChange;
+
+    @FXML private TextField TextF_I_O;
+    @FXML private TextField TF_Error;
+
+    @FXML private TextArea TextArea;
+
 
 }
