3c8fd50de12875d77ad7398c5e690cbec6eb723f
Mathias Gnadlinger <mathias.gnadlinger@htl-wels.at>
2020-12-22 20:32:20 +0100

    Nummern auslesen

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -4,9 +4,14 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import java.util.Stack;
 
 public class Controller
 {
+    private Stack<String> stack = new Stack<String>();
+
+    StringBuilder sb1 = new StringBuilder();                                        //StringBuilder for the TextField
+    
 
 
     @FXML private Button btn0;
@@ -37,5 +42,60 @@ public class Controller
 
     @FXML private TextArea TextArea;
 
+
+    public void inputNumbers()
+    {
+        if (btn0.isHover())
+        {
+            sb1.append(0);
+            TextF_I_O.setText(sb1.toString());
+        } else if (btn1.isHover())
+        {
+            sb1.append(1);
+            TextF_I_O.setText(sb1.toString());
+        } else if (btn2.isHover())
+        {
+            sb1.append(2);
+            TextF_I_O.setText(sb1.toString());
+        } else if (btn3.isHover())
+        {
+            sb1.append(3);
+            TextF_I_O.setText(sb1.toString());
+        } else if (btn4.isHover())
+        {
+            sb1.append(4);
+            TextF_I_O.setText(sb1.toString());
+        } else if (btn5.isHover())
+        {
+            sb1.append(5);
+            TextF_I_O.setText(sb1.toString());
+        } else if (btn6.isHover())
+        {
+            sb1.append(6);
+            TextF_I_O.setText(sb1.toString());
+        } else if (btn7.isHover())
+        {
+            sb1.append(7);
+            TextF_I_O.setText(sb1.toString());
+        } else if (btn8.isHover())
+        {
+            sb1.append(8);
+            TextF_I_O.setText(sb1.toString());
+        } else if (btn9.isHover())
+        {
+            sb1.append(9);
+            TextF_I_O.setText(sb1.toString());
+        }
+        else if(btnComma.isHover())
+        {
+            sb1.append(".");
+            TextF_I_O.setText(sb1.toString());
+        }
+        else if(btnPlus_Minus.isHover())
+        {
+            sb1.append("-");
+            TextF_I_O.setText(sb1.toString());
+        }
+    }
 
 }
